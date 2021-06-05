6bd8dc60550178aef331397ac838cf9adfbca7ed
farisawan.2000 <farisawan.2000@gmail.com>
2021-06-05 16:02:50 -0400

    used printf for sample because c++ overhead is cringe!

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main() {
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-    std::cout << extensionCount << " extensions supported\n";
+    printf("%d ext. supported", extensionCount);
 
     glm::mat4 matrix;
     glm::vec4 vec;
